c57c2595b69081eb578584a6a583460efa8d52a5
soviet <sovietxd@worlddev.ru>
2024-08-28 08:46:00 +0300

    Update vite.config.js

--- packages/builder/vite.config.js
+++ packages/builder/vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	optimizeDeps: {
     	include: ["html-minifier-terser", "clean-css"]
- 	 },
+ 	},
+ 	define: {
+		'process.env': {},
+		'process.platform': JSON.stringify(process.platform)
+	}
 	test: {
 		include: ['./src/**/*.{test,spec}.{js,ts}']
 	}
